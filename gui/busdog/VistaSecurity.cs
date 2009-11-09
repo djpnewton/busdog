5c392008596f3059baef7260c648260a140f98eb
djpnewton <djpnewton@gmail.com>
2009-11-09 07:23:19 +0000

    holy crap, the 0.1 branch merge failed to add this file to svn

---
+++ gui/busdog/VistaSecurity.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Security.Principal;
+
+namespace busdog
+{
+    public static class VistaSecurity
+    {
+        [DllImport("user32")]
+        public static extern UInt32 SendMessage(IntPtr hWnd, UInt32 msg, UInt32 wParam, UInt32 lParam);
+
+        internal const int BCM_FIRST = 0x1600;
+        internal const int BCM_SETSHIELD = (BCM_FIRST + 0x000C);
+
+        static internal bool IsVistaOrHigher()
+        {
+            return Environment.OSVersion.Version.Major < 6;
+        }
+
+        /// <summary>
+        /// Checks if the process is elevated
+        /// </summary>
+        /// <returns>If is elevated</returns>
+        static internal bool IsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal p = new WindowsPrincipal(id);
+            return p.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        /// <summary>
+        /// Add a shield icon to a button
+        /// </summary>
+        /// <param name="b">The button</param>
+        static internal void AddShieldToButton(Button b)
+        {
+            b.FlatStyle = FlatStyle.System;
+            SendMessage(b.Handle, BCM_SETSHIELD, 0, 0xFFFFFFFF);
+        }
+
+        /// <summary>
+        /// Restart the current process with administrator credentials
+        /// </summary>
+        internal static void RestartElevated(string args)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = Application.ExecutablePath;
+            startInfo.Arguments = args;
+            startInfo.Verb = "runas";
+            try
+            {
+                Process p = Process.Start(startInfo);
+            }
+            catch(System.ComponentModel.Win32Exception)
+            {
+                return; //If cancelled, do nothing
+            }
+
+            Application.Exit();
+        }
+    }
+}
