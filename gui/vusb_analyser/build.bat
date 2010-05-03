10d80737fa4f463f7f975a612c38da42e3d739b2
djpnewton <djpnewton@gmail.com>
2010-05-03 09:22:08 +0000

    * fixed up vusb-analyzer build batch file

--- gui/vusb_analyser/build.bat
+++ gui/vusb_analyser/build.bat
@@ -1,19 +1,23 @@
+@set VUSB_SRC=vusb_src
+
 :: checkout vusb-analyzer source
 if exist vusb_src (
-    svn update vusb_src
+    svn update %VUSB_SRC%
 ) else (
-    svn co https://vusb-analyzer.svn.sourceforge.net/svnroot/vusb-analyzer/trunk vusb_src
+    svn co https://vusb-analyzer.svn.sourceforge.net/svnroot/vusb-analyzer/trunk %VUSB_SRC%
 )
 
 :: copy files
-copy setup.py vusb_src
-copy cairo_port.patch vusb_src
+copy setup.py %VUSB_SRC%
+copy cairo_port.patch %VUSB_SRC%
 
 :: change dir
-cd vusb_src
+cd %VUSB_SRC%
 
 :: apply patch
 svn revert *
+svn revert VUsbTools\*
+del VUsbTools\Canvas.py
 patch -p0 -i cairo_port.patch
 
 :: run py2exe
